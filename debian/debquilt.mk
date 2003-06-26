73c094c3b43fcfc5fdb5856108f08ca45bcb1120
Martin Quinson <mquinson@debian.org>
2003-06-26 13:42:46 +0000

    Makefile chunk to manage debian packages using quilt

---
+++ debian/debquilt.mk
@@ -0,0 +1,13 @@
+#!/usr/bin/make -f
+
+# Common rules to handle a debian package using the quilt patch utility
+patch:
+	[ -L patches ] || ln -s debian/patches patches
+	quilt push -a
+
+unpatch:
+	if [ -L patches ] ; then \
+	  quilt pop -a; \
+	  rm patches; \
+	fi
+	rm -rf .pc
