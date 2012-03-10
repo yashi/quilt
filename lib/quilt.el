yashi/quilt

commit a106d99d754c88c049c111f6ccdfb06c2bede4f2

quilt-el: cleanup codes by introducing `quilt-cmd-to-list' function.

lib/quilt.el
@@ -96,20 +96,21 @@
 	  (shell-command-to-string (concat "quilt " cmd))
 	(cd d)))))
 
-(defun quilt-applied-list ()
-  (let ((s (quilt-cmd-to-string "applied")))
+(defun quilt-cmd-to-list (cmd)
+  "execute a quilt command at the top of the quilt tree for the current buffer.
+Return the lines of the command output as elements of a list."
+  (let ((s (quilt-cmd-to-string cmd)))
     (if s
-	(split-string s "\n"))))
+        (split-string s "\n" t))))
+
+(defun quilt-applied-list ()
+  (quilt-cmd-to-list "applied"))
 
 (defun quilt-file-list ()
-  (let ((s (quilt-cmd-to-string "files")))
-    (if s
-	(split-string s "\n"))))
+  (quilt-cmd-to-list "files"))
 
 (defun quilt-patch-list ()
-  (let ((s (quilt-cmd-to-string "series")))
-     (if s
-	 (split-string s "\n"))))
+  (quilt-cmd-to-list "patches"))
 
 (defun quilt-top-patch ()
   (if (quilt-bottom-p)

commit 93527eb0b96e565cd3da1ce08f9288469bb4dd44

quilt-el: fix `quilt-patch-list' to call `quilt series'..
It's a regression problem caused by a106d99d754c88c049c111f6ccdfb06c2bede4f2.

lib/quilt.el
@@ -126,7 +126,7 @@
 
 (defun quilt-patch-list ()
   "Return the list of the name of patches."
-  (quilt-cmd-to-list "patches"))
+  (quilt-cmd-to-list "seriess"))
 
 (defun quilt-files-affected (&optional first last)
   "Return the file names which modified from FIRST to LAST."