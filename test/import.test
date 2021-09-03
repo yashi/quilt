yashi/quilt

commit c539338458e4aa8b2d6c4c87b9192def3800d0b5

Delay sourcing patchfns until options have been processed
Commit 8b39a960afcf ("Consistently complain early if no series file
is found") unexpectedly broke commands like "quilt add -h", because
the check for a series file happens before the command line options
are processed. This, in turn, breaks the generation of the quilt
manual page, which relies on the output of each command when called
with option -h.

Reorder the code so that options are always processed first, and then
patchfns is sourced. That way, option "-h" will work again even if
not in a quilt working tree.

Fixes: 8b39a960afcf ("Consistently complain early if no series file is found")
Signed-off-by: Jean Delvare <jdelvare@suse.de>

quilt/import.in
@@ -9,14 +9,6 @@
 # One of the few commands which does not need a series file
 skip_series_check=1
 
-# Read in library functions
-if ! [ -r $QUILT_DIR/scripts/patchfns ]
-then
-	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
-	exit 1
-fi
-. $QUILT_DIR/scripts/patchfns
-
 usage()
 {
 	printf $"Usage: quilt import [-p num] [-R] [-P patch] [-f] [-d {o|a|n}] patchfile ...\n"
@@ -134,7 +126,7 @@ while true
 do
 	case "$1" in
 	-P)
-		opt_patch=${2#$QUILT_PATCHES/}
+		opt_patch=$2
 		shift 2 ;;
 	-p)
 		opt_strip=$2
@@ -165,6 +157,15 @@ then
 	exit 1
 fi
 
+# Read in library functions
+if ! [ -r $QUILT_DIR/scripts/patchfns ]
+then
+	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
+	exit 1
+fi
+. $QUILT_DIR/scripts/patchfns
+
+[ -n "$opt_patch"] && opt_patch=${opt_patch#$QUILT_PATCHES/}
 [ -n "$opt_strip" ] && patch_args="-p$opt_strip"
 if [ -n "$opt_reverse" ]
 then

quilt/add.in
@@ -6,14 +6,6 @@
 #
 #  See the COPYING and AUTHORS files for more details.
 
-# Read in library functions
-if ! [ -r $QUILT_DIR/scripts/patchfns ]
-then
-	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
-	exit 1
-fi
-. $QUILT_DIR/scripts/patchfns
-
 usage()
 {
 	printf $"Usage: quilt add [-P patch] {file} ...\n"
@@ -84,6 +76,14 @@ if [ $# -lt 1 ]
 then
 	usage
 fi
+
+# Read in library functions
+if ! [ -r $QUILT_DIR/scripts/patchfns ]
+then
+	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
+	exit 1
+fi
+. $QUILT_DIR/scripts/patchfns
 
 patch=$(find_applied_patch "$opt_patch") || exit 1
 

quilt/annotate.in
@@ -6,14 +6,6 @@
 #
 #  See the COPYING and AUTHORS files for more details.
 
-# Read in library functions
-if ! [ -r $QUILT_DIR/scripts/patchfns ]
-then
-	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
-	exit 1
-fi
-. $QUILT_DIR/scripts/patchfns
-
 usage()
 {
 	printf $"Usage: quilt annotate [-P patch] {file}\n"
@@ -109,6 +101,15 @@ if [ $# -ne 1 ]
 then
 	usage
 fi
+
+# Read in library functions
+if ! [ -r $QUILT_DIR/scripts/patchfns ]
+then
+	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
+	exit 1
+fi
+. $QUILT_DIR/scripts/patchfns
+
 opt_file="$SUBDIR$1"
 
 opt_patch=$(find_applied_patch "$opt_patch") || exit 1

quilt/applied.in
@@ -6,14 +6,6 @@
 #
 #  See the COPYING and AUTHORS files for more details.
 
-# Read in library functions
-if ! [ -r $QUILT_DIR/scripts/patchfns ]
-then
-	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
-	exit 1
-fi
-. $QUILT_DIR/scripts/patchfns
-
 usage()
 {
 	printf $"Usage: quilt applied [patch]\n"
@@ -53,6 +45,14 @@ if [ $# -gt 1 ]
 then
 	usage
 fi
+
+# Read in library functions
+if ! [ -r $QUILT_DIR/scripts/patchfns ]
+then
+	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
+	exit 1
+fi
+. $QUILT_DIR/scripts/patchfns
 
 patch=$(find_applied_patch "$1") || exit 1
 

quilt/delete.in
@@ -6,14 +6,6 @@
 #
 #  See the COPYING and AUTHORS files for more details.
 
-# Read in library functions
-if ! [ -r $QUILT_DIR/scripts/patchfns ]
-then
-	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
-	exit 1
-fi
-. $QUILT_DIR/scripts/patchfns
-
 usage()
 {
 	printf $"Usage: quilt delete [-r] [--backup] [patch|-n]\n"
@@ -73,6 +65,14 @@ if [ $# -gt 1 -o \( -n "$opt_next" -a $# -gt 0 \) ]
 then
 	usage
 fi
+
+# Read in library functions
+if ! [ -r $QUILT_DIR/scripts/patchfns ]
+then
+	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
+	exit 1
+fi
+. $QUILT_DIR/scripts/patchfns
 
 if [ -n "$1" ]; then
 	patch=$(find_patch "$1") || exit 1

quilt/diff.in
@@ -6,16 +6,6 @@
 #
 #  See the COPYING and AUTHORS files for more details.
 
-# Read in library functions
-if ! [ -r $QUILT_DIR/scripts/patchfns ]
-then
-	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
-	exit 1
-fi
-. $QUILT_DIR/scripts/patchfns
-
-setup_colors
-
 usage()
 {
 	printf $"Usage: quilt diff [-p n|-p ab] [-u|-U num|-c|-C num] [--combine patch|-z] [-R] [-P patch] [--snapshot] [--diff=utility] [--no-timestamps] [--no-index] [--sort] [--color[=always|auto|never]] [file ...]\n"
@@ -156,12 +146,7 @@ do
 		shift 2 ;;
 	--combine)
 		opt_combine=1
-		if [ "$2" = - ]
-		then
-			first_patch=-
-		else
-			first_patch=$(find_applied_patch "$2") || exit 1
-		fi
+		first_patch=$2
 		shift 2 ;;
 	-R)
 		opt_reverse=1
@@ -211,6 +196,21 @@ do
 		break ;;
 	esac
 done
+
+# Read in library functions
+if ! [ -r $QUILT_DIR/scripts/patchfns ]
+then
+	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
+	exit 1
+fi
+. $QUILT_DIR/scripts/patchfns
+
+setup_colors
+
+if [ -n "$first_patch" -a "$first_patch" != - ]
+then
+	first_patch=$(find_applied_patch "$first_patch") || exit 1
+fi
 
 QUILT_DIFF_OPTS="$QUILT_DIFF_OPTS $opt_format"
 

quilt/edit.in
@@ -8,14 +8,6 @@
 
 : ${EDITOR:=vi}
 
-# Read in library functions
-if ! [ -r $QUILT_DIR/scripts/patchfns ]
-then
-	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
-	exit 1
-fi
-. $QUILT_DIR/scripts/patchfns
-
 usage()
 {
 	printf $"Usage: quilt edit file ...\n"
@@ -55,6 +47,14 @@ if [ $# -eq 0 ]
 then
 	usage
 fi
+
+# Read in library functions
+if ! [ -r $QUILT_DIR/scripts/patchfns ]
+then
+	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
+	exit 1
+fi
+. $QUILT_DIR/scripts/patchfns
 
 quilt_command add "$@"
 status=$?

quilt/files.in
@@ -6,14 +6,6 @@
 #
 #  See the COPYING and AUTHORS files for more details.
 
-# Read in library functions
-if ! [ -r $QUILT_DIR/scripts/patchfns ]
-then
-	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
-	exit 1
-fi
-. $QUILT_DIR/scripts/patchfns
-
 usage()
 {
 	printf $"Usage: quilt files [-v] [-a] [-l] [--combine patch] [patch]\n"
@@ -69,7 +61,7 @@ do
 		then
 			:
 		else
-			first_patch=$(find_patch_in_series "$2") || exit 1
+			first_patch=$2
 		fi
 		shift 2 ;;
 	--)
@@ -81,6 +73,19 @@ done
 if [ $# -gt 1 ]
 then
 	usage
+fi
+
+# Read in library functions
+if ! [ -r $QUILT_DIR/scripts/patchfns ]
+then
+	echo "Cannot read library $QUILT_DIR/scripts/patchfns" >&2
+	exit 1
+fi
+. $QUILT_DIR/scripts/patchfns
+
+if [ -n "$first_patch" ]
+then
+	first_patch=$(find_patch_in_series "$first_patch") || exit 1
 fi
 
 last_patch=$(find_patch_in_series "$1") || exit 1

commit 62d53007cc96d488f1b108118bbb6568f93041e8

import: Fix option -P
I broke "import -P" with my previous commit. Using this option now
fails with the following error message:
/usr/share/quilt/import: line 168: [: missing `]'

This is caused by a missing space before the closing square bracket,
which prevents bash from parsing the line.

Fix that, and add a test case to cover this code path, so that no
such bug can creep in in the future.

Signed-off-by: Jean Delvare <jdelvare@suse.de>
Fixes: c539338458e4 ("Delay sourcing patchfns until options have been processed")

quilt/import.in
@@ -165,7 +165,7 @@ then
 fi
 . $QUILT_DIR/scripts/patchfns
 
-[ -n "$opt_patch"] && opt_patch=${opt_patch#$QUILT_PATCHES/}
+[ -n "$opt_patch" ] && opt_patch=${opt_patch#$QUILT_PATCHES/}
 [ -n "$opt_strip" ] && patch_args="-p$opt_strip"
 if [ -n "$opt_reverse" ]
 then

test/import.test
@@ -255,3 +255,19 @@
 	> Importing patch ../empty.patch (stored as %{_P}empty.patch)
 	$ quilt import %{PWD}/empty2.patch
 	> Importing patch %{PWD}/empty2.patch (stored as %{_P}empty2.patch)
+
+# Test importing under a different name
+	$ cd ..
+	$ quilt delete -rn
+	> Removed patch %{P}empty2.patch
+	$ quilt delete -rn
+	> Removed patch %{P}empty.patch
+	$ quilt import t/patch1.diff -P patch1-renamed.diff
+	> Importing patch t/patch1.diff (stored as %{P}patch1-renamed.diff)
+	$ quilt push
+	> Applying patch %{P}patch1-renamed.diff
+	> patching file f
+	>
+	> Now at patch patches/patch1-renamed.diff
+	$ cat f
+	> f